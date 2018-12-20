19924f4cf48f6040f04f6e18129065a2659ee44d
Monica <monicalopezgris@gmail.com>
2018-12-20 16:18:06 +0100

    add obstacle

--- js/class/game.js
+++ js/class/game.js
@@ -6,6 +6,7 @@ class Game {
     this.context = this.canvas.getContext('2d');
     this.dino = new Dino(50, 20, "red", 50, 430, this.context); //width, height, color, x, y, ctx
     this.ground = new Ground(20, 500, "black", 0, 480, this.context);
+    this.obstacle = new Obstacle(20, 20, "grey", 300, 460, this.context);
     //this.interval = setInterval(updateGame,500);
     
   }
