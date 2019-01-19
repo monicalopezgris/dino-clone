37421115b19a1ff700a39ba16fdd0efeefb0e896
Monica Lopez Gris <monicalopezgris@gmail.com>
2019-01-19 13:15:07 +0100

    update dino jump, can jump multiple times

--- js/class/game.js
+++ js/class/game.js
@@ -8,9 +8,30 @@ class Game {
     this.context = this.canvas.getContext("2d");
     this.context.fillStyle = "blue";
     this.context.fillRect(this.posX, this.posY, this.height, this.width);
-    this.ground = new Ground(40, this.windowWidth, "green", 0, this.windowHeight-40, this.context);
-    this.dino = new Dino(20, 50, "img/dino.png", 50, this.windowHeight - this.ground.width - 50, this.context); //width, height, color, x, y, ctx
-    this.obstacle = new Obstacle(20, 20, "grey", this.windowWidth - 20, this.windowHeight - this.ground.width, this.context); //width, height, color, x, y, ctx
+    this.ground = new Ground(
+      80,
+      this.windowWidth,
+      "green",
+      0,
+      this.windowHeight - 80,
+      this.context
+    );
+    this.dino = new Dino(
+      20,
+      50,
+      "img/dino.png",
+      50,
+      this.windowHeight - this.ground.width - 50,
+      this.context
+    ); //width, height, color, x, y, ctx
+    this.obstacle = new Obstacle(
+      20,
+      20,
+      "grey",
+      this.windowWidth - 20,
+      this.windowHeight - this.ground.width,
+      this.context
+    ); //width, height, color, x, y, ctx
     this.obstacles = [];
     this.keys = [];
     this.generatingObstacle = false;
@@ -27,12 +48,46 @@ class Game {
 
   clear() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    
+  }
+
+  randomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
   }
 
   // ------------------------OBSTACLES--------------------------------
   createObstacle() {
-    this.obstacles.push(new Obstacle(20, 20, "grey", this.windowWidth - 20, this.windowHeight - this.ground.width -20, this.context));
-    console.log(this.obstacles);
+    let object = this.randomNumber(0, 1);
+
+    switch (object) {
+      case 0:
+        this.obstacles.push(
+          new Obstacle(
+            20,
+            20,
+            "grey",
+            this.windowWidth - 20,
+            this.windowHeight - this.ground.width - 20,
+            this.context
+          )
+        );
+        break;
+      case 1:
+        this.obstacles.push(
+          new Obstacle(
+            20,
+            20,
+            "red",
+            this.windowWidth - 20,
+            this.windowHeight - this.ground.width - 30,
+            this.context
+          )
+        );
+        break;
+      default:
+        break;
+    }
+
     this.generatingObstacle = false;
   }
 
@@ -43,7 +98,7 @@ class Game {
 
   deleteObstacle() {
     this.obstacles.forEach(obstacle => {
-      if (obstacle.posX < -20) {
+      if (obstacle.x < -20) {
         this.obstacles = this.obstacles.shift();
       }
     });
@@ -66,51 +121,68 @@ class Game {
     window.addEventListener(
       "keydown",
       function(e) {
-        this.keys[e.keyCode] = true;
+        if (this.keys[38]) {
+          this.keys[e.keyCode] = false;
+        } else {
+          this.keys[e.keyCode] = true;
+        }
       }.bind(this),
       false
     );
     window.addEventListener(
       "keyup",
       function(e) {
-        this.keys[e.keyCode] = false;
+        if (this.keys[38]) {
+          this.keys[e.keyCode] = true;
+        } else {
+          this.keys[e.keyCode] = false;
+        }
       }.bind(this),
       false
     );
 
     if (this.keys[37]) {
       this.dino.moveLeft();
+     
     }
 
     if (this.keys[39]) {
       this.dino.moveRight();
+     
     }
 
-    if (this.keys[38]) {
-      if (this.dino.jumping === false) {
-        this.dino.jump();
-        this.dino.autoLand();
-      } else {
+    /*if (this.keys[38]) {
+      this.dino.jump(this.ground)
+      console.log(this.dino.inGround)
+      if (this.dino.inGround){
+        this.keys[38] = false;
       }
-    }
+    }*/
 
-    if (this.keys[40]) {
-      this.dino.land();
-    }
+    /*if (this.keys[40]) {
+      this.dino.land()
+    }*/
   }
 
-  drawDino() {
-    a;
+  onKeyUpDinoJump() {
+    document.onkeyup = function(e) {
+      if (e.keyCode === 38) {
+        this.dino.jump(this.ground)
+        // this.dino.inGround = false;
+      
+      }
+    }.bind(this);
   }
+
 
   // ------------------------COLISIONS--------------------------------
 
   collisionControl() {
     this.obstacles.forEach(obstacle => {
       if (
-        this.dino.posY + this.dino.height >= obstacle.posY &&
-        this.dino.posX + this.dino.width > obstacle.posX &&
-        this.dino.posX <= obstacle.posX + obstacle.height
+        this.dino.y + this.dino.height >= obstacle.y &&
+        this.dino.x + this.dino.width > obstacle.x &&
+        this.dino.x <= obstacle.x + obstacle.height
       ) {
         setStage("game-over", "start");
         this.gameOver = true;
@@ -123,8 +195,17 @@ class Game {
     if (!this.gameOver) {
       this.clear();
       this.ground.paint();
+      this.onKeyUpDinoJump();
       this.dinoMove();
+      
+      if (this.dino.inGround ===false) {
+        this.dino.jump();
+      }
+
+      console.log(this.keys)
+      
       this.dino.paint();
+      this.dino.landControl(this.ground);
       
       if (this.generatingObstacle === false) {
         this.randomCreateObstacle();
@@ -138,6 +219,7 @@ class Game {
         this.deleteObstacle();
       }
       
+      //console.log(this.keys)
       if (this.interval !== undefined) {
         this.interval = window.requestAnimationFrame(this.update.bind(this));
       }
--- js/class/obstacle.js
+++ js/class/obstacle.js
@@ -2,23 +2,21 @@ class Obstacle {
   constructor(width, height, color, x, y, ctx) {
     this.width = width;
     this.height = height;
-    this.posX = x;
-    this.posY = y;
+    this.x = x;
+    this.y = y;
+    this.vx = 1;
     this.ctx = ctx;
-    this.color= color;
-    this.velocity = 1;
+    this.color = color;
     this.ctx.fillStyle = this.color;
-    this.ctx.fillRect(this.posX, this.posY, this.height, this.width);
+    this.ctx.fillRect(this.x, this.y, this.height, this.width);
   }
 
   paint() {
     this.ctx.fillStyle = this.color;
-    this.ctx.fillRect(this.posX, this.posY, this.height, this.width);
-    console.log('updating')
+    this.ctx.fillRect(this.x, this.y, this.height, this.width);
   }
 
-  move(){
-    this.posX -= this.velocity;
+  move() {
+    this.x -= this.vx;
   }
-
 }
